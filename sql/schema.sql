ca30b1296dc74d00ff63d1abe324c3c4923a8015
ikirezidorcas04-byte <ikirezidorcas04@gmail.com>
2026-06-29 07:19:35 -0700

    add customer table

---
+++ sql/schema.sql
@@ -0,0 +1,11 @@
+--customer table
+CREATE TABLE customer (
+    customer_id INT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone_number VARCHAR(15),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
